eac78b68871e162526a64e45524c40df124b2df3
Mauro Carabotti <carabotti@puzzle.ch>
2013-05-13 18:58:24 +0200

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sentido::Application.config.secret_token = '5879eda43ce6ee60f962811a278cf183095db1f689ad0143d7797b3c1a50c6a6b38e7bd2cf3c9e4c780794637ead7c0a13cd7dc663bf9c4016d6ed1bfcabd730'
