12be8fbc9169abe693e65a1e8d022a9c86c57781
Mauro Carabotti <carabotti@puzzle.ch>
2013-05-16 22:43:46 +0200

    seed from gpx

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,35 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alonside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# '<,'>s/\(\d*\.\d*\),\(\d\.\d*\)/Coordinate.create( lat: \1, lon: \2)/
 
 Trail.delete_all
 
-first_trail = Trail.create( 
-  name: 'Moutier', 
-  path: Path.new( coordinates: [
- [7.371805, 47.28485],
- [7.371665, 47.284369],
- [7.371987, 47.284223],
- [7.372159, 47.284092],
- [7.373159, 47.284092],
- [7.374159, 47.284392]]))
+count = 0
 
-first_trail.save!
+file_loc = 'db/gpx'
+
+Dir.glob(file_loc + '/*.gpx') do |gpx|
+  
+  coordinates = []
+
+  File.open(gpx, "r"){ |file| 
+    file.readlines.each_with_index do |line, idx|
+
+      line.gsub(/lat="(.*)" lon="(.*)"/) do
+
+        coord = [$2.to_f, $1.to_f]
+
+        coordinates << coord
+
+      end
+    end
+
+
+    Trail.create( name: gpx.gsub(/#{file_loc}\/(.*)\.gpx/, '\\1'),
+                 path: Path.new( coordinates: coordinates)).save!
+
+                 
+     count += 1
+  }
+end
+
+puts "Inserted #{count} trails"
