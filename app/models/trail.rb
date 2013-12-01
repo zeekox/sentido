3a0b891af15a332a962351d42bcb43b249db8ce1
Mauro Carabotti <zeekox@gmail.com>
2013-12-01 20:52:05 +0100

    make seed use csv to get data

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,11 +1,27 @@
 class Trail
   include MongoMapper::Document
 
+  key :code, String
+
   key :name, String
+
+  key :date, Date
 
   key :l_val, Float
 
   key :length, String
+  
+  key :state, String
+  
+  key :diff, String
+  
+  key :region, String
+  
+  key :comment, String
+
+  key :map, String
+
+  key :down, Boolean
   
   one :path
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,49 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alonside the db with db:setup).
+#
+# Numéro;Date Modif;Nom du Sentier;Etat;Difficulté;Km;Sous_Régions;Lien carte;Nbre secteurs;Tous secteurs=même auteur;Date sentier terminé;Remarque;Carte 25'000;Trace gps;r7;Montée
+#
 
 require 'gpx_helper'
 require 'distance_helper'
+require 'csv'
 
 Trail.delete_all
 
 count = 0
 
-file_loc = 'db/gpx'
+file_loc = 'db/gpx/'
 
-Dir.glob(file_loc + '/*.gpx') do |gpx|
+CSV.foreach('db/DataAccess.csv', {:col_sep => ';'}) do |row|
 
-  coordinates = GpxHelper.parse_gpx(gpx)
+  gpx = file_loc + row[0] + '.gpx'
 
-  l_val = DistanceHelper.compute_total(coordinates)
-  
-  length = DistanceHelper.to_display(l_val)
+  if File.exist?(gpx)
+    coordinates = GpxHelper.parse_gpx(gpx)
+    l_val = DistanceHelper.compute_total(coordinates)
+    length = DistanceHelper.to_display(l_val)
+    puts "#{gpx} #{coordinates.length} coordinates, length #{length}"
 
-  puts "#{gpx} #{coordinates.length} coordinates, length #{length}"
+    date = nil
+    if row[1]
+      date = Date.strptime(row[1], '%m/%d/%Y')
+    end   
 
+    Trail.create(code: row[0],
+                 name: row[2],
+                 date: (date or ''),
+                 state: (row[3] or ''),
+                 diff: (row[4] or ''),
+                 region: (row[6] or ''),
+                 comment: (row[11] or ''),
+                 map: (row[12] or ''),
+                 down: row[15] && row[15].downcase == 'non',
+                 length: length,
+                 l_val: l_val,
+                 path: Path.new( coordinates: coordinates)).save!
 
-  Trail.create( name: gpx.gsub(/#{file_loc}\/(.*)\.gpx/, '\\1'),
-               length: length,
-               l_val: l_val,
-               path: Path.new( coordinates: coordinates)).save!
-
-
-  count += 1
+    count += 1
+  end
 end
 
 puts "Inserted #{count} trails"
