f307082146c0d2e990c29c89d4886f33f6d8c0c1
Mauro Carabotti <zeekox@gmail.com>
2014-02-01 17:20:57 +0100

    fix mongo.rb init

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,22 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "sentido-#{Rails.env}"
+#include MongoMapper
+
+db_config = YAML::load(File.read(File.join(Rails.root, "/config/mongo.yml")))
+#
+if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
+  mongo = db_config[Rails.env]
+
+  if mongo['host'] && mongo['port']
+    MongoMapper.connection = Mongo::Connection.new(mongo['host'], mongo['port'],:logger => Rails.logger)
+  elseif mongo['uri']
+    MongoMapper.connection = Mongo::Connection.new(mongo['uri'],:logger => Rails.logger)
+  end
+
+  MongoMapper.database = mongo['sentido-#{Rails.env']
+
+  if mongo['username'] && mongo['password']
+    MongoMapper.database.authenticate(mongo['username'], mongo['password'])
+  end
+end
 
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
