4bdc8fc1043f38c0f505418de8c65a7b6884fc8d
Mauro Carabotti <zeekox@gmail.com>
2014-05-03 19:51:26 +0200

    script to setup new machine and small change in package.json

---
+++ setup_dev_machine.sh
@@ -0,0 +1,9 @@
+#mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install --yes mongodb-org
+
+echo "filling the local db"
+rake db:seed
+
