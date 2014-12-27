891a991b024aa4633fdaed4165257d7912c4ec15
Mauro Carabotti <zeekox@gmail.com>
2014-12-27 16:02:03 +0100

    authentication using omniauth google oauth2
    based on http://richonrails.com/articles/google-authentication-in-ruby-on-rails and adapted to work with mongo_mapper

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User
+  include MongoMapper::Document
+  def self.from_omniauth(auth)
+    find_or_create_by_provider_and_uid(auth.provider, auth.uid).tap do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
+  key :uid, String
+  key :name, String
+  key :email, String
+  key :provider, String
+  key :oauth_token, String
+  key :oauth_expires_at, Time
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Sentido::Application.routes.draw do
-  get "trails/index"
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+
+  root to: "home#show"
+
+  get "trails/index"
   get "trails/show"
+  
   resources :trails
 
   match '/trails/around/:sw_lon/:sw_lat/:ne_lon/:ne_lat' => 'trails#around', :constraints => {:sw_lon => /\-*\d+.\d+/ , :sw_lat => /\-*\d+.\d+/, :ne_lon => /\-*\d+.\d+/ , :ne_lat => /\-*\d+.\d+/}
