f1ec42def73e83355a96ac57a726deb6f920484f
Mauro Carabotti <zeekox@gmail.com>
2015-01-03 11:43:44 +0100

    replaced oauth secret key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '181717409328-kocpk45ke2ch9575p94u2o5h2chdk9a9.apps.googleusercontent.com', 'K_1lIsINrI-8UFucLFuVNOE6', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '181717409328-kocpk45ke2ch9575p94u2o5h2chdk9a9.apps.googleusercontent.com', 'n25t7kzA1ZFKGWfHaY1p7Hr7', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
