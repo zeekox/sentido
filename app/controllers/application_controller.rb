483f8a5fa5c036dd798dea0bed5270a79a0b1805
Mauro Carabotti <carabotti@puzzle.ch>
2016-05-24 15:57:42 -0500

    include proper CORS headers to allow requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  skip_before_filter :verify_authenticity_token
+
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
@@ -13,5 +18,23 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     puts "You are not authorized to perform this action."
     render :json => []
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'localhost'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = 'localhost'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
   end
 end
